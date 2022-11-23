cab4907c9969017c6086505938fdb56c9e2fccac
KuanWeiChen <chen20020316@gmail.com>
2022-11-23 17:30:32 +0800

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by KuanWei on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
